3e24256d4263cbb914ef9fc8c41aadf54e391a72
Jose <jlaresti93@gmail.com>
2024-05-16 21:35:33 +0200

    Creando las paginas y aplicando el routing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import router from './routes/index.js';
+import express from "express";
+import router from "./routes/index.js";
 
 const app = express();
 
@@ -7,12 +7,22 @@ const app = express();
 const port = process.env.port || 4000;
 
 //Habilitar PUG
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
+
+//Obtener el año actual
+app.use((req, res, next) => {
+   const year = new Date();
+   res.locals.actualYear = year.getFullYear();
+   res.locals.nombreSitio = 'Agencia de viajes';
+    next();
+});
+
+//Definir la carpeta publica
+app.use(express.static("public"));
 
 //Agregar Router
-app.use('/', router);
-
+app.use("/", router);
 
 app.listen(port, () => {
-    console.log(`El servidor esta funcionando en el puerto ${port}`);
-})
+  console.log(`El servidor esta funcionando en el puerto ${port}`);
+});
