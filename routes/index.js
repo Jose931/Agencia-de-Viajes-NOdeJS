5bfc37a25c15cc4dc5fbd7fabb9b7041ec24c9b4
Jose <jlaresti93@gmail.com>
2024-05-17 23:28:11 +0200

    Pagina de viaje y el formulario de testimoniales

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ import {
   paginaTestimoniales,
   paginaDetalleViaje
 } from "../controllers/paginasController.js";
+import { guardarTestimonial } from "../controllers/testimonialController.js";
 
 const router = express.Router();
 
@@ -17,5 +18,6 @@ router.get("/viajes", paginaViajes);
 router.get("/viajes/:slug", paginaDetalleViaje);
 
 router.get("/testimoniales", paginaTestimoniales);
+router.post("/testimoniales",guardarTestimonial);
 
 export default router;
