6c4f9db8d7420e0873931264c4e22681db238146
Jose <jlaresti93@gmail.com>
2024-05-18 09:53:50 +0200

    Proyecto acabado. Cambiar los datos de la BD antes del deploy

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -1,10 +1,26 @@
 import { Viajes } from "../models/Viaje.js";
 import { Comentario } from "../models/Comentarios.js";
 
-const paginaInicio = (req, res) => {
-  res.render("inicio", {
-    pagina: "Inicio",
-  });
+const paginaInicio = async (req, res) => {
+  //Consultar 3 viajes del modelo viaje
+
+  const promiseDB = [];
+
+  promiseDB.push(Viajes.findAll({ limit: 3 }));
+  promiseDB.push(Comentario.findAll({ limit: 3 }));
+
+  try {
+    const resultado = await Promise.all(promiseDB);
+
+    res.render("inicio", {
+      pagina: "Inicio",
+      clase: "home",
+      viajes: resultado[0],
+      comentarios: resultado[1],
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const paginaNosotros = (req, res) => {
@@ -14,43 +30,47 @@ const paginaNosotros = (req, res) => {
 };
 
 const paginaViajes = async (req, res) => {
-    //Consultar base de datos
-    const viajes = await Viajes.findAll();
+  //Consultar base de datos
+  const viajes = await Viajes.findAll();
 
   res.render("viajes", {
     pagina: "Próximos Viajes",
-    viajes: viajes
+    viajes: viajes,
   });
 };
 
 const paginaComentarios = async (req, res) => {
-
   try {
     const comentarios = await Comentario.findAll();
     res.render("comentarios", {
       pagina: "Comentarios",
-      comentarios
+      comentarios,
     });
   } catch (error) {
     console.log(error);
   }
-  
 };
 
 //Muestr un viaje por su slug
 const paginaDetalleViaje = async (req, res) => {
-    const {slug} = req.params;
+  const { slug } = req.params;
 
-    try {
-        const viaje = await Viajes.findOne({where: { slug }});
+  try {
+    const viaje = await Viajes.findOne({ where: { slug } });
 
-        res.render('viaje', {
-            pagina: 'Información viaje',
-            viaje
-        })
-    } catch (error) {
-        console.log(error);        
-    }
-}
+    res.render("viaje", {
+      pagina: "Información viaje",
+      viaje,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export { paginaInicio, paginaNosotros, paginaViajes, paginaComentarios,paginaDetalleViaje };
+export {
+  paginaInicio,
+  paginaNosotros,
+  paginaViajes,
+  paginaComentarios,
+  paginaDetalleViaje,
+};
